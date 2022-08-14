5f2379ec064684d40387cd8d12809573e3327062
Devesh Biswal <103762012+DeveshBiswal@users.noreply.github.com>
2022-08-14 13:17:11 +0000

    Initial commit

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Anything you want
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
