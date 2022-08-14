8210d3fd40298d0d6814c0eaff5ed802bf74e76a
Devesh Biswal <103762012+DeveshBiswal@users.noreply.github.com>
2022-08-14 16:31:43 +0000

    Deleting unneccessary files and codes

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -2,22 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Anything you want
-        </a>
-      </header>
-    </div>
+   <div className="App">
+      
+   </div>
   );
 }
-
+ 
 export default App;
